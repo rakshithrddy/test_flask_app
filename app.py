a7d2c0e337e42fca31adaa58f9872fdb4b5df1fb
Rakshith N <rakshith.n@atos.net>
2023-04-11 17:10:17 +0530

    added stylesheet with bootstrap and about us and contact page

--- app.py
+++ app.py
@@ -8,5 +8,13 @@ def home():
     # Render the home page template and pass in the list of cars
     return render_template('home.html')
 
+@app.route('/about_us')
+def about_us():
+    return render_template('about_us.html')
+
+@app.route('/contact_us')
+def contact_us():
+    return render_template('contact_us.html')
+
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
