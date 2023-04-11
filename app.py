8927cefd415a551ae861653d2e324e3f18745972
Rakshith N <rakshith.n@atos.net>
2023-04-11 11:25:05 +0530

    updated requirements.txtf file

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     # Render the home page template and pass in the list of cars
-    return render_template('home.html', cars=cars)
+    return render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
